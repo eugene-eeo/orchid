9bea04f1c2ee37b7cedfc1f2d5d70701e89f2add
Eeo Jun <141bytes@gmail.com>
2018-01-21 20:19:28 +0000

    update

---
+++ app.go
@@ -0,0 +1,169 @@
+package main
+
+import "fmt"
+import "bytes"
+import "os"
+import "github.com/nsf/termbox-go"
+import "github.com/eugene-eeo/hubwub/player"
+import "github.com/lucasb-eyer/go-colorful"
+import "github.com/eliukblau/pixterm/ansimage"
+
+func nextTrack(p *player.Player, i int, force bool, q chan func(*player.Player)) {
+	d, err := p.Next(i, force)
+	if err != nil {
+		fmt.Println(err)
+		p.Remove()
+		go func() {
+			q <- func(p *player.Player) {
+				nextTrack(p, 1, true, q)
+			}
+		}()
+		return
+	}
+	go (func() {
+		graceful := <-d
+		if !graceful {
+			return
+		}
+		q <- func(p *player.Player) {
+			nextTrack(p, 1, false, q)
+		}
+	})()
+}
+
+func getIndicator(p *player.Player) string {
+	if p.Stream != nil && p.Stream.Paused() {
+		return "Ⅱ"
+	}
+	if p.Shuffle {
+		return "⥮"
+	}
+	return "⏵"
+}
+
+func main() {
+	songs, err := player.FindSongs(".")
+	if err != nil {
+		os.Exit(1)
+	}
+	app := player.NewPlayer(songs)
+	/*
+		+-------+
+		|       | <Prev>
+		| 16x16 | <Now Playing>
+		|       | <Up Next>
+		+-------+
+	*/
+
+	must(termbox.Init())
+	termbox.SetOutputMode(termbox.Output256)
+	defer termbox.Close()
+
+	exit := make(chan struct{})
+	requests := make(chan func(*player.Player))
+
+	drawName := func(name string, y int, color termbox.Attribute) {
+		unicodeCells(fit(name, 30), func(dx int, r rune) {
+			termbox.SetCell(18+dx, y, r, color, termbox.ColorDefault)
+		})
+	}
+
+	updateQueue := func(app *player.Player) {
+		for i := 1; i <= 3; i++ {
+			s, err := app.Peek(i)
+			if err != nil {
+				break
+			}
+			drawName(s.Name(), 2+i, 0xf0)
+		}
+		s, err := app.Peek(-1)
+		if err != nil {
+			return
+		}
+		drawName(s.Name(), 1, 0xf0)
+	}
+
+	imageQueue := make(chan player.Song, 1)
+
+	go (func() {
+		var currentSong player.Song = player.Song("")
+		var image *ansimage.ANSImage = nil
+		for {
+			sng := <-imageQueue
+			if sng != currentSong {
+				currentSong = sng
+				r, ok := sng.Picture()
+				if !ok {
+					image = nil
+					continue
+				}
+				bg, _ := colorful.Hex("#000000")
+				image, err = ansimage.NewScaledFromReader(bytes.NewReader(r), 16, 16, bg, ansimage.ScaleModeResize, ansimage.NoDithering)
+				if err != nil {
+					image = nil
+					continue
+				}
+			}
+			if image != nil {
+				termbox.SetCursor(0, 0)
+				must(termbox.Sync())
+				print(image.Render())
+				print("\u001B[?25l")
+			}
+		}
+	})()
+
+	render := func(app *player.Player) {
+		must(termbox.Clear(termbox.ColorDefault, termbox.ColorDefault))
+		color := termbox.Attribute(0x1ff)
+		if app.Repeat {
+			color = termbox.AttrReverse
+		}
+		s, err := app.Song()
+		name := "<No songs>"
+		if err == nil {
+			name = s.Name()
+		}
+		drawName(getIndicator(app)+" "+name, 2, color)
+		updateQueue(app)
+		must(termbox.Sync())
+		if err == nil {
+			imageQueue <- s
+		}
+	}
+
+	go (func() {
+		for {
+			r := <-requests
+			r(app)
+			render(app)
+		}
+	})()
+
+	if len(app.Queue) > 0 {
+		requests <- func(app *player.Player) { nextTrack(app, 0, true, requests) }
+	}
+	go (func() {
+		for {
+			evt := termbox.PollEvent()
+			switch evt.Ch {
+			case 'q':
+				exit <- struct{}{}
+				break
+			case 'n':
+				requests <- func(h *player.Player) { nextTrack(app, 1, true, requests) }
+			case 'p':
+				requests <- func(h *player.Player) { nextTrack(app, -1, true, requests) }
+			case 's':
+				requests <- func(h *player.Player) { h.ToggleShuffle() }
+			case 'r':
+				requests <- func(h *player.Player) { h.ToggleRepeat() }
+			}
+			if evt.Key == termbox.KeySpace {
+				requests <- func(h *player.Player) { h.Toggle() }
+			}
+		}
+	})()
+
+	<-exit
+}
---
+++ player/player.go
@@ -0,0 +1,164 @@
+package player
+
+import "os"
+import "strings"
+import "errors"
+import "math/rand"
+import "sort"
+
+var NoMoreSongs error = errors.New("No more songs")
+
+func FindSongs(dir string) (songs []Song, err error) {
+	f, err := os.Open(dir)
+	if err != nil {
+		return
+	}
+	files, err := f.Readdirnames(-1)
+	if err != nil {
+		return
+	}
+	songs = []Song{}
+	for _, name := range files {
+		if strings.HasSuffix(name, ".mp3") {
+			songs = append(songs, Song(name))
+		}
+	}
+	return
+}
+
+func remove(i int, xs []int) []int {
+	return append(xs[:i], xs[i+1:]...)
+}
+
+func mod(r int, m int) int {
+	t := r % m
+	if t < 0 {
+		t += m
+	}
+	return t
+}
+
+func seq(n int) []int {
+	b := make([]int, n)
+	for i := 0; i < n; i++ {
+		b[i] = i
+	}
+	return b
+}
+
+func shuffle(xs []int, c int) int {
+	n := len(xs)
+	m := c
+	for i := 0; i < n; i++ {
+		j := rand.Intn(n)
+		xs[i], xs[j] = xs[j], xs[i]
+		if c == xs[i] {
+			m = i
+		}
+		if c == xs[j] {
+			m = j
+		}
+	}
+	return m
+}
+
+type Player struct {
+	Shuffle bool
+	Repeat  bool
+	Stream  *Stream
+	cursor  int
+	order   []int
+	Queue   []Song
+}
+
+func NewPlayer(songs []Song) *Player {
+	return &Player{
+		Shuffle: false,
+		cursor:  0,
+		order:   seq(len(songs)),
+		Queue:   songs,
+	}
+}
+
+func (p *Player) ToggleRepeat() {
+	p.Repeat = !p.Repeat
+}
+
+func (p *Player) ToggleShuffle() {
+	p.Shuffle = !p.Shuffle
+	if p.Shuffle {
+		p.cursor = shuffle(p.order, p.cursor)
+	} else {
+		p.cursor = p.order[p.cursor]
+		sort.Ints(p.order)
+	}
+}
+
+func (p *Player) Song() (Song, error) {
+	return p.Peek(0)
+}
+
+func (p *Player) Next(i int, force bool) (chan bool, error) {
+	if len(p.order) == 0 {
+		return nil, NoMoreSongs
+	}
+	if !p.Repeat || force {
+		p.cursor += i
+	}
+	p.cursor = mod(p.cursor, len(p.order))
+	return p.Play()
+}
+
+func (p *Player) Peek(i int) (Song, error) {
+	if len(p.order) == 0 {
+		return Song(""), NoMoreSongs
+	}
+	return p.Queue[p.order[mod(p.cursor+i, len(p.order))]], nil
+}
+
+func (p *Player) play(done chan bool) (*Stream, error) {
+	s, err := p.Song()
+	if err != nil {
+		return nil, err
+	}
+	stream, err := s.Stream(func(graceful bool) {
+		go func() {
+			done <- graceful
+			close(done)
+		}()
+	})
+	if err != nil {
+		return stream, err
+	}
+	return stream, stream.Play()
+}
+
+func (p *Player) Play() (done chan bool, err error) {
+	if p.Stream != nil {
+		p.Stream.Teardown(false)
+	}
+	done = make(chan bool)
+	stream, err := p.play(done)
+	if err != nil {
+		if stream != nil {
+			stream.Teardown(false)
+			<-done
+		}
+		p.Stream = nil
+		return
+	}
+	p.Stream = stream
+	return
+}
+
+func (p *Player) Remove() {
+	if len(p.Queue) > 0 {
+		p.order = remove(p.cursor, p.order)
+	}
+}
+
+func (p *Player) Toggle() {
+	if p.Stream != nil {
+		p.Stream.Toggle()
+	}
+}
