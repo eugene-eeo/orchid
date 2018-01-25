c4c9a277637bcdb8c58d5c48cbe3250cdb0d9fc6
Eeo Jun <141bytes@gmail.com>
2018-01-25 19:56:00 +0000

    more tests

--- elems/input_test.go
+++ elems/input_test.go
@@ -1,5 +1,6 @@
 package elems_test
 
+import "strings"
 import "math/rand"
 import "reflect"
 import "testing"
@@ -29,6 +30,7 @@ func (i Int100) Generate(rand *rand.Rand, size int) reflect.Value {
 
 // Insert(a), Delete(b) => max(a-b, 0)
 // Insert(a), Move(b), Insert(c) => a+c
+// Insert(a), Move(b), Delete(c) => max(a-b-c,0) + min(a,b)
 
 func TestInputInsertDelete2(t *testing.T) {
 	err := quick.Check(func(x Int100, y Int100) bool {
@@ -39,7 +41,7 @@ func TestInputInsertDelete2(t *testing.T) {
 		for i := 0; i < int(y); i++ {
 			input.Delete()
 		}
-		return len(input.String()) == max(int(x-y), 0)
+		return input.String() == strings.Repeat("k", max(int(x-y), 0))
 	}, nil)
 	if err != nil {
 		t.Error(err)
@@ -48,16 +50,38 @@ func TestInputInsertDelete2(t *testing.T) {
 
 func TestInputInsertMove(t *testing.T) {
 	err := quick.Check(func(x Int100, y Int100, z Int100) bool {
+		a := int(x)
+		b := int(y)
+		c := int(z)
 		input := elems.NewInput()
-		for i := 0; i < int(x); i++ {
+		for i := 0; i < a; i++ {
+			input.Insert('a')
+		}
+		input.Move(-int(b))
+		for i := 0; i < c; i++ {
+			input.Insert('c')
+		}
+		target := strings.Repeat("a", max(a-b, 0)) +
+			strings.Repeat("c", c) +
+			strings.Repeat("a", min(b, a))
+		return input.String() == target && input.Cursor() == max(a-b, 0)+int(c)
+	}, nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestInputInsertMoveDelete(t *testing.T) {
+	err := quick.Check(func(a Int100, b Int100, c Int100) bool {
+		input := elems.NewInput()
+		for i := 0; i < int(a); i++ {
 			input.Insert('k')
 		}
-		input.Move(-int(y))
-		for i := 0; i < int(z); i++ {
-			input.Insert('k')
+		input.Move(-int(b))
+		for i := 0; i < int(c); i++ {
+			input.Delete()
 		}
-		return len(input.String()) == int(x+z) &&
-			input.Cursor() == max(int(x-y), 0)+int(z)
+		return len(input.String()) == max(int(a-b-c), 0)+min(int(a), int(b))
 	}, nil)
 	if err != nil {
 		t.Error(err)
